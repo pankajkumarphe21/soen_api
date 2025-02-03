87d863effc9ed6ce9835e2f9aad09c7b0d978c45
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-03 18:19:59 +0530

    initiate

--- app.js
+++ app.js
@@ -7,14 +7,12 @@ import cors from 'cors'
 import projectRoutes from './routes/project.routes.js'
 import roomRoutes from './routes/room.routes.js';
 
+app.use(cors());
 connect();
 
 const app=express();
 
 // app.use(morgan('dev'));
-app.use(cors({
-    origin: 'http://localhost:5174' // Allow only this origin
-  }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
