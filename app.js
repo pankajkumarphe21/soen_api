3287438479e77fb458fe1e662e5815cb0b421863
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-03 18:21:42 +0530

    initiate

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ import cors from 'cors'
 import projectRoutes from './routes/project.routes.js'
 import roomRoutes from './routes/room.routes.js';
 
-app.use(cors());
-connect();
 
 const app=express();
+app.use(cors());
 
+connect();
 // app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
