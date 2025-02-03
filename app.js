b2d946633c6e8f0ca7d3a827f1549ad9bffba5fc
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-03 19:27:15 +0530

    initiate

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
-app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
+app.use(cors({origin:process.env.FRONTEND_URL || 'http://localhost:5173',credentials:true}));
 
 connect();
 // app.use(morgan('dev'));
