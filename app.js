ebf5a0bc61d9f3f79616a35608acd49a521149e8
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-03 19:20:51 +0530

    initiate

--- app.js
+++ app.js
@@ -12,17 +12,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
-app.use(cors({origin:'*'}));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "http://localhost:5173");
-    
-    if (req.method === "OPTIONS") {
-        return res.status(200).json({});
-    }
-
-    next();
-});
-
+app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
 
 connect();
 // app.use(morgan('dev'));
